05ff45d3bf480df9c9345f539e598182b7eb86a4
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-04-09 19:43:51 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,10 +30,10 @@ end
 
 def hit?(int)
   prompt_user
-  get_user_input
-  if get_user_input == "s"
+  input = get_user_input
+  if input == "s"
 
-  elsif get_user_input == "h"
+  elsif input == "h"
     deal_card
   else
     invalid_command
